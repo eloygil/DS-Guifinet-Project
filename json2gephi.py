529802086b7a1dc1d7b05413c787931978fd541b
Eloy Gil <eloy@eloygil.com>
2017-04-18 23:55:43 +0200

    First commit
    Starting JSON2Gephi parser

---
+++ json2gephi.py
@@ -0,0 +1,32 @@
+# Python Json Parser by Eloy Gil & Hao Wu
+# {eloy.gil,hao.wu}@ac.upc.edu
+# MIRI - DS - FIB (UPC)
+import json
+with open('captura.json', 'r') as f:
+	nodelist = []    # List of nodes (id, label, long, latitude)
+	edgelist = []	 # List of links between nodes (origin, dest, signal, bandwidth)
+	for row in f:
+		data = json.loads(row)
+	for key, value in data.items():
+		if key == "nodes":
+			for keys, values in value.items():
+				label = lat = lon = 'null'
+				for keys_node, value_node in values.items():
+					if keys_node == "name":
+						label = value_node
+					if keys_node == "lon":
+						lon = value_node
+					if keys_node == "lat":
+						lat = value_node
+						nodelist.append([keys,label,lon,lat])
+					if keys_node == "links":
+						bw = signal = 'null'
+						for key_links, value_links in value_node.items():
+							for key_link, value_link in value_links.items():
+								if key_link == "signal":
+									signal = value_link
+								if key_link == "bandwidth":
+									bw = value_link
+									edgelist.append([keys,key_links,signal,bw])
+	#print nodelist[0:5]
+	#print edgelist[0:5]
