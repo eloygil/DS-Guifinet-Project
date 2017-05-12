8f1bc40d38c268203efaf295632bef73bfbeb03d
HaoWu28 <hao.wu@est.fib.upc.edu>
2017-05-12 11:39:18 +0200

    update commit

--- json2gephi.py
+++ json2gephi.py
@@ -2,44 +2,56 @@
 # {eloy,hao.wu}@ac.upc.edu
 # MIRI - DS - FIB (UPC)
 import json
-with open('captura.json', 'r') as f:
-	nodelist = []    # List of nodes (id, label, long, latitude)
-	edgelist = []	 # List of links between nodes (origin, dest, signal, bandwidth)
-	for row in f:
-		data = json.loads(row)
-	for key, value in data.items():
-		if key == "nodes":
-			for keys, values in value.items():
-				label = lat = lon = 'null'
-				for keys_node, value_node in values.items():
-					if keys_node == "name":
-						label = value_node
-					if keys_node == "lon":
-						lon = value_node
-					if keys_node == "lat":
-						lat = value_node
-						nodelist.append([keys,label,lon,lat])
-					if keys_node == "links":
-						bw = signal = 'null'
-						for key_links, value_links in value_node.items():
-							for key_link, value_link in value_links.items():
-								if key_link == "signal":
-									signal = value_link
-								if key_link == "bandwidth":
-									bw = value_link
-									edgelist.append([keys,key_links,signal,bw])
+def print_to_csv(i):
+	name=i+'.txt'
+	with open(name, 'r') as f:
+		nodelist = []    # List of nodes (id, label, long, latitude)
+		edgelist = []	 # List of links between nodes (origin, dest, signal, bandwidth)
+		for row in f:
+			data = json.loads(row)
+		for key, value in data.items():
+			if key == "nodes":
+				for keys, values in value.items():
+					label = lat = lon = 'null'
+					for keys_node, value_node in values.items():
+						if keys_node == "name":
+							label = value_node
+						if keys_node == "lon":
+							lon = value_node
+						if keys_node == "lat":
+							lat = value_node
+							nodelist.append([keys,label,lon,lat])
+						if keys_node == "links":
+							bw = signal = 'null'
+							for key_links, value_links in value_node.items():
+								for key_link, value_link in value_links.items():
+									if key_link == "signal":
+										signal = value_link
+									if key_link == "bandwidth":
+										bw = value_link
+										edgelist.append([keys,key_links,signal,bw])
 
 	#to print cvs edges
 	#print ("Source;Target;Type;Weight")
-	#for p in edgelist: 
-	#	 print ("{d[0]};{d[1]};Directed;{d[2]}".format(d=p))
-
+		f = open('csv_'+i+'_edges.csv', 'w')
+		f.write ("Source;Target;Type;Weight\n")
+		for p in edgelist: 
+			f.write("{d[0]};{d[1]};Directed;{d[2]}\n".format(d=p))
+		f.close()
 	#toprint cvs nodes
-	print("Id;Label")	
+	#print("Id;Label")	
+		f = open('csv_'+i+'_nodes.csv', 'w')
+		f.write ("Id;Label;City;Latitude;Longitude\n")
 
-	for p in nodelist: 
-		 print ("{d[0]};{d[1]}".format(d=p))
+		for p in nodelist: 
+			f.write ("{d[0]};{d[1]};Barcelona;{d[3]};{d[2]}\n".format(d=p))
 
+		f.close()
+	
+
+for x in range(0, 141):
+#for x in range(0, 1):
+    print_to_csv(str(x))
 
 	#print ("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
 	#print ("<gexf xmlns:viz=\"http:///www.gexf.net/1.1draft/viz\" version=\"1.1\" xmlns=\"http://www.gexf.net/1.1draft\">")
