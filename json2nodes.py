f5675533b1d9d105048615dc97acef51bf8bc36d
Eloy Gil <eloy@eloygil.com>
2017-05-14 21:56:12 +0200

    fix done (now showing number of 1hour interval failures for each node)

--- json2nodes.py
+++ json2nodes.py
@@ -8,8 +8,9 @@ import operator
 nodelist = []
 failed = []
 down_nodes = []
-for x in range(0, 140):
+for x in range(0, 141):
     name = str(x) + '.txt'
+    #print name
     with open(name) as data_file:
         data = json.load(data_file)
     if x == 0:
@@ -25,12 +26,16 @@ for x in range(0, 140):
                 else:
                     failed.append(str(node))
                     down_nodes.remove(str(node))
+        for down_node in down_nodes:
+            if down_node not in data["nodes"]:
+                failed.append(str(down_node))
         for old_node in nodelist:
             if old_node not in data["nodes"]:
                 nodelist.remove(str(old_node))
+                failed.append(str(old_node))
                 down_nodes.append(str(old_node))
 frequency = {i:failed.count(i) for i in failed}
 sorted_freq = sorted(frequency.items(), key=operator.itemgetter(1), reverse=True)
-print 'Failed nodes list, ordered by number of failures:'
+print 'Failed nodes list, ordered by number of 1-hour downtime failures:'
 for failed_node in sorted_freq:
     print 'Node ' + str(failed_node[0]) + ' = ' + str(failed_node[1])
